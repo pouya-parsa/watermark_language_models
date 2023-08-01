87a47b78b91575153c1a72f45d64553df2ace182
Pouya Parsa <mail.pouyaparsa@gmail.com>
2023-08-01 04:30:53 +0330

    feat: add WatermarkDetectorClass

--- summarizer.py
+++ summarizer.py
@@ -1,15 +1,17 @@
+import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 from transformers import (LogitsProcessorList)
 import sys
 sys.path.insert(0, "/content/lm-watermarking")
 from watermark_processor import WatermarkDetector, WatermarkLogitsProcessor
 
-model = T5ForConditionalGeneration.from_pretrained('t5-small')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = T5ForConditionalGeneration.from_pretrained('t5-small').to(device)
 tokenizer = T5Tokenizer.from_pretrained('t5-small', model_max_length=3072)
 
 def summarize(text, watermark=False, gamma=0.25, delta=2.0):
   if not watermark:
-    inputs = tokenizer(["summarize: " + text], return_tensors="pt")
+    inputs = tokenizer(["summarize: " + text], return_tensors="pt").to(device)
     summary = model.generate(**inputs, return_dict_in_generate=True, max_new_tokens=1000, output_scores=True)
     summary = tokenizer.batch_decode(summary.sequences, skip_special_tokens=True)[0]
   else:
@@ -17,15 +19,9 @@ def summarize(text, watermark=False, gamma=0.25, delta=2.0):
                                                gamma=gamma,
                                                delta=delta,
                                                seeding_scheme="simple_1")
-    tokenized_input = tokenizer(text, return_tensors="pt").to(model.device)
-    print(tokenized_input["input_ids"].shape)
+    tokenized_input = tokenizer(["summarize: " + text], return_tensors="pt").to(device)
     output_tokens = model.generate(**tokenized_input,
                                   logits_processor=LogitsProcessorList([watermark_processor]), max_new_tokens=1000, return_dict_in_generate=True, output_scores=True)
     summary = tokenizer.batch_decode(output_tokens.sequences, skip_special_tokens=True)[0]
    
   return summary
-
-# "len(summary.sequences[0]): ", len(summary.sequences[0])
-
-# print("len(summary.scores): ", len(summary.scores))
-# print("summary.scores[1].shape: ", summary.scores[1].shape)
--- utils.py
+++ utils.py
@@ -1,7 +1,24 @@
+import sys
 import lmppl
+sys.path.insert(0, "/content/lm-watermarking")
+from watermark_processor import WatermarkDetector, WatermarkLogitsProcessor
 
-scorer = lmppl.EncoderDecoderLM('google/flan-t5-large')
+scorer = lmppl.EncoderDecoderLM('google/flan-t5-small')
 
 def get_perplexity(input_sentences, output_sentences):
   ppl = scorer.get_perplexity(input_texts=input_sentences, output_texts=output_sentences)
   return ppl
+
+class WatermarkDetectorClass():
+  def __init__(self, tokenizer, gamma, device): 
+    self.watermark_detector = WatermarkDetector(vocab=list(tokenizer.get_vocab().values()),
+                                            gamma=gamma, # should match original setting
+                                            seeding_scheme="simple_1", # should match original setting
+                                            device=device, # must match the original rng device type
+                                            tokenizer=tokenizer,
+                                            z_threshold=4.0,
+                                            normalizers=[],
+                                            ignore_repeated_bigrams=False)
+  def detect(self, text):
+    score_dict = self.watermark_detector.detect(text) # or any other text of interest to analyze
+    return score_dict
